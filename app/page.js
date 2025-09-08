f6621b0c0ae3584687ff727b0d08ed634995deef
Mahesh <ramahesh024@gmail.com>
2025-09-09 04:22:27 +0530

    feat: animation and hero section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import NavBar from "@/components/navbar/navbar";
 import Footer from "@/components/footer/footer";
+import HeroSection from "@/components/herosection/herosection";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <NavBar />
+      <HeroSection />
       <Footer />
     </div>
   );
