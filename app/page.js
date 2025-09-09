652f4262a2856d9aa11190ced872e26474719712
Epanagandla Asha Bibi <e.ashabibi123@gmail.com>
2025-09-09 10:05:35 +0530

    pushing

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import styles from "./page.module.css";
 import NavBar from "@/components/navbar/navbar";
+import Footer from "@/components/footer/footer";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <NavBar />
+      <Footer />
+      
     </div>
   );
 }
---
+++ components/footer/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './footer.module.css'
+export default function footer() {
+  return (
+    <div className={styles.footer}>
+      <p>Footer content goes here</p>
+    </div>
+  )
+}
